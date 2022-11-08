849d1e118a819ccd5f8ae1c9db8b76e21a92e2c5
aluno <aluno@PC003920.iul.intra>
2022-11-08 10:31:49 +0000

    update 2

--- ES/src/main/java/CalendarioFenix.java
+++ ES/src/main/java/CalendarioFenix.java
@@ -3,8 +3,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-
+import java.io.File;
 import javax.net.ssl.HttpsURLConnection;
+import java.io.FileWriter;
 
 public class CalendarioFenix {
 
@@ -19,6 +20,8 @@ public class CalendarioFenix {
 		BufferedReader br = new BufferedReader(isr);
 
 		String inputLine;
+		File jsonInes = new File("jsonInes.txt");
+		FileWriter writer = new FileWriter(jsonInes);
 		String jsonStringI = "[";
 		boolean isInsideEvent = false;
 
@@ -26,25 +29,33 @@ public class CalendarioFenix {
 			if(inputLine.compareTo("BEGIN:VEVENT")==0) {
 				jsonStringI = jsonStringI +"{"  + inputLine + "," ;
 				isInsideEvent = true;
+				writer.write(jsonStringI);
 			}
 			else if (inputLine.contains("DTSTART:")) {
 				jsonStringI = jsonStringI + inputLine + "," ;
+				writer.write(jsonStringI);
 			}
 			else if (inputLine.contains("DTEND:")) {
 				jsonStringI = jsonStringI + inputLine + "," ;
+				writer.write(jsonStringI);
 			}
 			else if (inputLine.contains("SUMMARY:")) {
 				jsonStringI = jsonStringI + inputLine + "," ;
+				writer.write(jsonStringI);
 			}
 			else if (inputLine.contains("LOCATION:")) {
 				jsonStringI = jsonStringI + inputLine + "," ;
+				writer.write(jsonStringI);
 			}
 			else if(inputLine.compareTo("END:VEVENT")==0 && isInsideEvent) {
 				jsonStringI = jsonStringI + inputLine + "}";
+				writer.write(jsonStringI);
 			}
 		}
 		jsonStringI += "]";
-		System.out.println(jsonStringI);
+		writer.write("]");
+//		System.out.println(jsonStringI);
 		br.close();
+		writer.close();
 	}
 }
