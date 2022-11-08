77752767086e4e192978f7705e4bdad53340f76e
aluno <aluno@PC003920.iul.intra>
2022-11-08 10:27:29 +0000

    update CalendarioFenix

--- ES/src/main/java/CalendarioFenix.java
+++ ES/src/main/java/CalendarioFenix.java
@@ -8,30 +8,43 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class CalendarioFenix {
 
-//	Class that fetches Fenix Calendar and converts into json
-	
+	//	Class that fetches Fenix Calendar and converts into json
+
 	public static void main(String[] args) throws IOException{
-		String httpsURL = "webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=isasa1@iscte.pt&password=zLCpZLCYvhVBeyUsaEsd4ZNeFKdfHPgYzim2ITS5INtWNQzHAKsk1gfaBLNnhnCLcc5WGxYDKqmBz9OAbulLqWAn2NDiHQn7kfyqZ4M8WQwcuiZXvAWb9Oz55wXjyeRc";
+		String httpsURL = "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=isasa1@iscte.pt&password=zLCpZLCYvhVBeyUsaEsd4ZNeFKdfHPgYzim2ITS5INtWNQzHAKsk1gfaBLNnhnCLcc5WGxYDKqmBz9OAbulLqWAn2NDiHQn7kfyqZ4M8WQwcuiZXvAWb9Oz55wXjyeRc";
 		URL myURL = new URL(httpsURL);
-		HttpsURLConnection con = (HttpsURLConnection)myURL.openConnection();
-		InputStream is = con.getInputStream();
+		HttpsURLConnection urlConnection = (HttpsURLConnection)myURL.openConnection();
+		InputStream is = urlConnection.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
 
 		String inputLine;
-		String jsonString = "[";
+		String jsonStringI = "[";
 		boolean isInsideEvent = false;
+
 		while ((inputLine = br.readLine()) != null) {
 			if(inputLine.compareTo("BEGIN:VEVENT")==0) {
-				jsonString = jsonString +"{";
+				jsonStringI = jsonStringI +"{"  + inputLine + "," ;
 				isInsideEvent = true;
 			}
+			else if (inputLine.contains("DTSTART:")) {
+				jsonStringI = jsonStringI + inputLine + "," ;
+			}
+			else if (inputLine.contains("DTEND:")) {
+				jsonStringI = jsonStringI + inputLine + "," ;
+			}
+			else if (inputLine.contains("SUMMARY:")) {
+				jsonStringI = jsonStringI + inputLine + "," ;
+			}
+			else if (inputLine.contains("LOCATION:")) {
+				jsonStringI = jsonStringI + inputLine + "," ;
+			}
 			else if(inputLine.compareTo("END:VEVENT")==0 && isInsideEvent) {
-				jsonString = jsonString + inputLine + "}";
+				jsonStringI = jsonStringI + inputLine + "}";
 			}
 		}
-		jsonString += "]";
-		System.out.println(jsonString);
+		jsonStringI += "]";
+		System.out.println(jsonStringI);
 		br.close();
 	}
 }
