5b63d141b103bba0e2156e40b53decf6fce1a793
isasa1 <isasa1@PC012341.iul.intra>
2022-11-10 14:00:58 +0000

    conexão URI Hugo

--- ES/src/main/java/CalendarioFenix.java
+++ ES/src/main/java/CalendarioFenix.java
@@ -12,44 +12,87 @@ public class CalendarioFenix {
 	//	Class that fetches Fenix Calendar and converts into json
 
 	public static void main(String[] args) throws IOException{
-		String httpsURL = "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=isasa1@iscte.pt&password=zLCpZLCYvhVBeyUsaEsd4ZNeFKdfHPgYzim2ITS5INtWNQzHAKsk1gfaBLNnhnCLcc5WGxYDKqmBz9OAbulLqWAn2NDiHQn7kfyqZ4M8WQwcuiZXvAWb9Oz55wXjyeRc";
-		URL myURL = new URL(httpsURL);
-		HttpsURLConnection urlConnection = (HttpsURLConnection)myURL.openConnection();
-		InputStream is = urlConnection.getInputStream();
+		String httpsURLI = "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=isasa1@iscte.pt&password=zLCpZLCYvhVBeyUsaEsd4ZNeFKdfHPgYzim2ITS5INtWNQzHAKsk1gfaBLNnhnCLcc5WGxYDKqmBz9OAbulLqWAn2NDiHQn7kfyqZ4M8WQwcuiZXvAWb9Oz55wXjyeRc";
+		URL iURL = new URL(httpsURLI);
+		HttpsURLConnection urlConnectionI = (HttpsURLConnection)iURL.openConnection();
+		InputStream is = urlConnectionI.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
+		
+		String httpsURLH = "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=hmsea@iscte.pt&password=S98lQuMJLj5SbJOigxwvkhW6NxnbFjvrqtznetteK3nR04ozgL7BgMqEOPKFzgLMcaiXi5Ygjral29BCRkBmF9V3FTdkrne6yYxETDRIEapjhD8tOiGR5pB811UH8Pti";
+		URL hURL = new URL(httpsURLH);
+		HttpsURLConnection urlConnectionH = (HttpsURLConnection)hURL.openConnection();
+		InputStream is2 = urlConnectionH.getInputStream();
+		InputStreamReader isr2 = new InputStreamReader(is2);
+		BufferedReader br2 = new BufferedReader(isr2);
+		
 
-		String inputLine;
+		String inputLineInes;
 		File jsonInes = new File("jsonInes.txt");
-		FileWriter writer = new FileWriter(jsonInes);
+		FileWriter writerInes = new FileWriter(jsonInes);
 		String jsonStringI = "[";
 		boolean isInsideEvent = false;
 
-		while ((inputLine = br.readLine()) != null) {
-			if(inputLine.compareTo("BEGIN:VEVENT")==0) {
-				jsonStringI = jsonStringI +"{"  + inputLine + "," ;
+		while ((inputLineInes = br.readLine()) != null) {
+			if(inputLineInes.compareTo("BEGIN:VEVENT")==0) {
+				jsonStringI = jsonStringI +"\n{\n"  + inputLineInes + ",\n" ;
 				isInsideEvent = true;
 			}
-			else if (inputLine.contains("DTSTART:")) {
-				jsonStringI = jsonStringI + inputLine + "," ;
+			else if (inputLineInes.contains("DTSTART:")) {
+				jsonStringI = jsonStringI + inputLineInes + ",\n" ;
 			}
-			else if (inputLine.contains("DTEND:")) {
-				jsonStringI = jsonStringI + inputLine + "," ;
+			else if (inputLineInes.contains("DTEND:")) {
+				jsonStringI = jsonStringI + inputLineInes + ",\n" ;
 			}
-			else if (inputLine.contains("SUMMARY:")) {
-				jsonStringI = jsonStringI + inputLine + "," ;
+			else if (inputLineInes.contains("SUMMARY:")) {
+				jsonStringI = jsonStringI + inputLineInes + ",\n" ;
 			}
-			else if (inputLine.contains("LOCATION:")) {
-				jsonStringI = jsonStringI + inputLine + "," ;
+			else if (inputLineInes.contains("LOCATION:")) {
+				jsonStringI = jsonStringI + inputLineInes + ",\n" ;
 			}
-			else if(inputLine.compareTo("END:VEVENT")==0 && isInsideEvent) {
-				jsonStringI = jsonStringI + inputLine + "}";
+			else if(inputLineInes.compareTo("END:VEVENT")==0 && isInsideEvent) {
+				jsonStringI = jsonStringI + inputLineInes + "\n}";
 			}
 		}
-		jsonStringI += "]";
-		writer.write(jsonStringI);
+		jsonStringI += "\n]";
+		writerInes.write(jsonStringI);
 //		System.out.println(jsonStringI);
 		br.close();
-		writer.close();
+		writerInes.close();
+		
+		
+		
+		String inputLineHugo;
+		File jsonHugo = new File("jsonHugo.txt");
+		FileWriter writerHugo = new FileWriter(jsonHugo);
+		String jsonStringH = "[";
+		boolean isInsideEvent2 = false;
+		
+		while ((inputLineHugo = br2.readLine()) != null) {
+			if(inputLineHugo.compareTo("BEGIN:VEVENT")==0) {
+				jsonStringH = jsonStringH +"\n{\n"  + inputLineHugo + ",\n" ;
+				isInsideEvent2 = true;
+			}
+			else if (inputLineHugo.contains("DTSTART:")) {
+				jsonStringH = jsonStringH + inputLineHugo + ",\n" ;
+			}
+			else if (inputLineHugo.contains("DTEND:")) {
+				jsonStringH = jsonStringH + inputLineHugo + ",\n" ;
+			}
+			else if (inputLineHugo.contains("SUMMARY:")) {
+				jsonStringH = jsonStringH + inputLineHugo + ",\n" ;
+			}
+			else if (inputLineHugo.contains("LOCATION:")) {
+				jsonStringH = jsonStringH + inputLineHugo + ",\n" ;
+			}
+			else if(inputLineHugo.compareTo("END:VEVENT")==0 && isInsideEvent2) {
+				jsonStringH = jsonStringH + inputLineHugo + "\n}";
+			}
+		}
+		jsonStringH += "\n]";
+		writerHugo.write(jsonStringH);
+//		System.out.println(jsonStringH);
+		br2.close();
+		writerHugo.close();
 	}
 }
